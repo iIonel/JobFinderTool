a9f5d016125a40627cc829f88420e0a4fdef96f8
iIonel <45739581+iIonel@users.noreply.github.com>
2024-07-05 22:17:32 +0300

    Delete application/src/main/resources directory

--- application/src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-create table jobs(
-    id int auto_increment,
-    job_link varchar(255) NOT NULL,
-    job_title varchar(255) NOT NULL,
-    experience varchar(255) NOT NULL,
-    job_type varchar(255) NOT NULL,
-    city varchar(255) NOT NULL,
-    company varchar(255) NOT NULL
-);
